b2005bc90c7fc2a77387a5e4b01063759699dbb6
James Adams <james@bluezen.co.uk>
2017-02-05 23:43:54 +0000

    Rewrite proof of concept using gstreamer
    The pygame mixer is nice and simple, but is (normally) limited to eight tracks
    and only supports a limited set of audio formats. While gstreamer is far more
    complex, it does not have these limitations.

--- mixer.py
+++ mixer.py
@@ -1,46 +1,66 @@
 #!/usr/bin/env python2
 
-from pygame import init, mixer, time
-from os.path import exists
-from sys import stdout
+from os.path import exists, dirname, realpath
+from sys import stdout, argv
 from gcd_lcm import lcm
+from pygame import time
+from gst import (
+    element_factory_make,
+    Format,
+    STATE_PLAYING,
+    STATE_NULL,
+    FORMAT_TIME,
+)
 
-def load_sounds(sound_files, bpm):
+def make_channel(channel_number):
+    channel = element_factory_make("playbin2", "channel%02d" % channel_number)
+    fakesink = element_factory_make("fakesink", "fakesink")
+    channel.set_property("video-sink", fakesink)
+    bus = channel.get_bus()
+    return channel, bus
+
+
+def make_channels(channel_count):
+    channels = []
+    buses = {}
+    for i in range(channel_count):
+        channel, bus = make_channel(i)
+        channels.append(channel)
+        buses[bus] = channel
+    return channels, buses
+
+
+def load_sounds(sound_files, bpm, channels):
     bps = bpm/60.
-    sounds = []
     timings = {}
-    for sf in sound_files:
+    time_format = Format(FORMAT_TIME)
+    for i, sf in enumerate(sound_files):
         if exists(sf):
-            sound = mixer.Sound(sf)
-            if sound:
-                sounds.append(sound)
-                length = sound.get_length()
-                # Round beats to nearest even number
-                # (this is generally the right thing to do)
-                beats = int(round(length/2. * bps)*2)
-                if beats:
-                    print 'added sound of length %5.1f seconds => %3d beats' % (length, beats)
-                    if beats not in timings:
-                        timings[beats] = []
-                        timings[beats].append(sound)
-                else:
-                    print 'E: Skipping %s, zero length' % sf
-            else:
-                print 'E: Failed to open %s' % sf
-        else:
-            print 'E: Will not open %s, does not exist' % sf
-    return sounds, timings
+            channel = channels[i]
+            channel.set_property('uri', u'file://%s/%s' % (cwd, sf))
+            channel.set_state(STATE_PLAYING)
+            channel.get_state()
+            length = channel.query_duration(time_format)[0] / 10.**9
+            channel.set_state(STATE_NULL)
+            channel.get_state()
+            # Round beats to nearest even number
+            # (this is generally the right thing to do)
+            beats = int(round(length/2. * bps)*2)
+            if beats not in timings:
+                timings[beats] = []
+            timings[beats].append(channel)
+    return timings
 
 
 def main(sound_files, bpm):
     bps = bpm/60.
-    init()
     clock = time.Clock()
-    sounds, timings = load_sounds(sound_files, bpm)
+    channels, buses = make_channels(len(sound_files))
+    timings = load_sounds(sound_files, bpm, channels)
 
     max_time = lcm(*timings.keys())
 
-    print '%2d sounds loaded' % len(sounds)
+    print '%2d sounds loaded' % len(channels)
 
     for timer, soundlist in timings.iteritems():
         print "%2d sounds are %2d beats long" % (len(soundlist), timer)
@@ -54,10 +74,10 @@ def main(sound_files, bpm):
         clock.tick(bps)
         for timer, sounds in timings.iteritems():
             if ticker % timer == 0:
-                for sound in sounds:
-                    sound.stop()
-                    sound.play()
-                    sound.set_volume(0.75)
+                for channel in sounds:
+                    channel.set_state(STATE_NULL)
+                    channel.set_state(STATE_PLAYING)
+                    channel.set_property('volume', 0.75)
                     stdout.write('~')
             else:
                 stdout.write(' '*len(sounds))
@@ -67,8 +87,8 @@ def main(sound_files, bpm):
             ticker = 0
 
 if __name__ == '__main__':
+    cwd = realpath(dirname(argv[0]))
     with open('tracklist') as tracklist:
         tracks = tracklist.readlines()
         tracks = [t.strip() for t in tracks]
-        mixer.pre_init(frequency=32000)
         main(tracks, 100)
